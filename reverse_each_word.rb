93bfbe818d69939f4efcbf2675fc521be75c4f7b
Anmol Kaur <kaur.anmol26@gmail.com>
2020-04-24 22:57:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,12 @@
-def reverse_each_word(string)
-  convert_to_array = string.split(' ')
-  reversed_words=[]
-  convert_to_array.collect do |string|
-    reversed_words << string.reverse
-  end
-  reversed_string=reversed_words.join(' ')
-end
+# def reverse_each_word(string)
+#   convert_to_array = string.split(' ')
+#   reversed_words=[]
+#   convert_to_array.collect do |string|
+#     reversed_words << string.reverse
+#   end
+#   reversed_string=reversed_words.join(' ')
+# end
 
+def reverse_each_word(string)
+  sentence.split.collect{ |word| word.reverse }.join(" ")
+end
